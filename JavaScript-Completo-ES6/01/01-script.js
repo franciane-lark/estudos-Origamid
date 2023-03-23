656562bca251e877adedb07d0fec1f3605e237b3
Franciane Lark <franciane.lark019@gmail.com>
2023-03-23 12:23:23 -0400

    update declaracao de variaveis

---
+++ JavaScript-Completo-ES6/01/01-script.js
@@ -0,0 +1,7 @@
+const nome = "André";
+
+function somar(a,b) {
+  return a + b;
+}
+
+somar(2,4);
---
+++ JavaScript-Completo-ES6/02/01-script.js
@@ -0,0 +1,25 @@
+var nome = "André";
+let idade = 28;
+const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+//----------------------------------------
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+//----------------------------------------
+var sobrenome = "Rafael", 
+    cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+//----------------------------------------
+var semDefinir;
+
+console.log(semDefinir);
+
+//---------------------------------------
